1d334eb7d6eb979ab3bcc88ce30ef352c40ccfcc
Elson <elsonliang@gmail.com>
2021-12-06 06:29:57 -0500

    Rename modules

--- scraper.js
+++ scraper.js
@@ -1,6 +1,6 @@
 /* Puppeteer Scraping */
 
-const puppeteer = require("puppeteer-extra")
+const puppeteer = require('puppeteer');
 
 async function getProfileImage(username){
   /* https://devdocs.io/puppeteer/index#pageevaluatepagefunction-args */
@@ -9,7 +9,7 @@ async function getProfileImage(username){
 
   return new Promise(async(resolve, reject) =>{
       try {
-          const browser = await puppeteer.default.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"] })
+          const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"] })
           const page = await browser.newPage();
           const response = await page.goto(url);
   
